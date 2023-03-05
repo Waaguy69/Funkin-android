54394875919dad4acee69c9cb5fdae81f9bb20c4
Waaguy69 <122211736+Waaguy69@users.noreply.github.com>
2023-03-05 05:39:55 -0300

    Update Boyfriend.hx

--- source/Boyfriend.hx
+++ source/Boyfriend.hx
@@ -9,18 +9,16 @@ using StringTools;
 
 class Boyfriend extends Character
 {
-	// public var stunned:Bool = false;
+	public var startedDeath:Bool = false;
 
 	public function new(x:Float, y:Float, ?char:String = 'bf')
 	{
 		super(x, y, char, true);
 	}
 
-	public var startedDeath:Bool = false;
-
 	override function update(elapsed:Float)
 	{
-		if (!debugMode)
+		if (!debugMode && animation.curAnim != null)
 		{
 			if (animation.curAnim.name.startsWith('sing'))
 			{
